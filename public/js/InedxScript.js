60510332f7e0cb049d31e1e8aeb9944a6ed08035
ali zamani <azamani081@gmail.com>
2025-11-10 13:05:08 +0330

    change port in indexscript file

--- public/js/InedxScript.js
+++ public/js/InedxScript.js
@@ -3,7 +3,7 @@ document.getElementById("btn").addEventListener("click",async (e)=>{
         let searchBarValue = document.getElementById("searchBar").value;
  const {port} = require("./../../config/config");
         try{
-                await fetch(`http://localhost:${port}/searchBar`,{
+                await fetch(`http://localhost:9090/searchBar`,{
                 method:"POST",
                 body: new URLSearchParams({ data:searchBarValue })})
         }catch(err){
@@ -14,17 +14,15 @@ document.getElementById("btn").addEventListener("click",async (e)=>{
 
 const buttons = document.querySelectorAll('.btn-outline-primary');
 buttons.forEach(btn => {
-
         btn.addEventListener('click', async () => {
-       
                 const btnValue = btn.value;
                 try {
-                        await fetch(`http://localhost:${port}/Category`, {
+                        await fetch(`http://localhost:9090/Category?data=${btnValue}`, {
                         method: "GET",});
                 } catch (err) {
                         console.error("Fetch failed:", err);
                 }
-    console.log("Button value:", btnValue,port);
+    console.log("Button value:", btnValue);
   });
 });
 
